a39ce52865707c990ca4eff18af6906e07d26476
Ingrid <ingrid.g2154@gmail.com>
2024-07-28 15:04:26 -0300

    first commit

---
+++ CalculoPGTO/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+public class CalculoPGTO
+{
+
+    public static void Main()
+    {
+        //Lê os valores de entrada 
+
+        float valorSalario = float.Parse(Console.ReadLine());
+        float valorBeneficio = float.Parse(Console.ReadLine());
+
+        float valorImposto = 0;
+
+        //Calcula o valor do imposto para salarios de 0 até 1.100
+        if (valorSalario >= 0 && valorSalario <= 1100)
+        {
+            valorImposto = 0.05f * valorSalario;
+        }
+        else if (valorSalario >= 1100.01 && valorSalario <= 2500)
+        {
+            valorImposto = 0.10f * valorSalario;
+        }
+        else
+        {
+            valorImposto = 0.15f * valorSalario;
+        }
+
+        //Saída
+        float saida = valorSalario - valorImposto + valorBeneficio;
+        Console.WriteLine(saida.ToString("0.00"));
+
+    }
+}
